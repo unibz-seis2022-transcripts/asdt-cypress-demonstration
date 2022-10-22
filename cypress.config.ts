51045016b08bd20e5ba5c59834ba39d25f4fc474
Teilkasko <teilkasko@online.de>
2022-10-23 00:14:39 +0200

    add simple cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

83e8281c5ef210e5f42e0a6e592578d1d6d83a62
Teilkasko <teilkasko@online.de>
2022-11-01 16:50:04 +0100

    add test for conditional rendering

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,4 +2,11 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/');
   });
+
+  it('renders completed todos with strike through', () => {
+    cy.get('.todo-item').first().find('input').click();
+    cy.get('.todo-item')
+      .first()
+      .should('have.css', 'text-decoration', 'line-through solid rgb(0, 0, 0)');
+  });
 });
